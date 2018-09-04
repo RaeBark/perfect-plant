af4761ee652bbfd016a0b3e716d286e689ed9a0c
Rachel Barker <rachelbarker001@gmail.com>
2018-09-04 09:39:53 -0700

    fixs backgroud

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Cart from '../../components/Cart/Cart';
+import './CartPage.css'
 
 const CartPage = (props) => {
     return (
-        <div>
+        <div className="CartPage">
             {props.cart &&
                 <Cart cart={props.cart} handleRemoveItem={props.handleRemoveItem} handleAddItem={props.handleAddItem}/>
             }
